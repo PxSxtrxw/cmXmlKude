c640de6b1dee81c663f77d29f78572e0db68e40f
Paiva Pablo <pablopaiva070@gmail.com>
2024-07-19 09:42:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -145,5 +145,5 @@ const server = http.createServer((req, res) => {
 
 const PORT = 3004;
 server.listen(PORT, () => {
-  logger.info(`Servidor iniciado en http://localhost:${PORT}`);
+  console.log(`Servidor iniciado en http://localhost:${PORT}`);
 });
